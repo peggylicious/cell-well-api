2c778cd69ec2c2956b0b1c348208216767b96951
margaret ogbor <p.asq@live.com>
2023-11-20 08:14:14 +0100

    Add send email functionality

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,102 @@ const User = require("../model/user");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const jwtSecret = process.env.JWT_SECRET;
+require("dotenv").config()
+const UserVerification = require('../model/userVerification')
+const nodemailer = require('nodemailer')
+const {v4: uuidv4} = require('uuid');
+// const { GoogleApis } = require("googleapis");
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+// const sendMail = (to, subject, message) =>{
+//     const transporter = nodemailer.createTransport({
+//         // host: "smtp-mail.outlook.com",
+//         // port: 587,
+//         // secure: false, // STARTTLS
+
+//         service: 'gmail',
+//         // host: "smtp-mail.outlook.com",
+//         // port: 587,
+//         // tls: {
+//         //     ciphers: "SSLv3",
+//         //     rejectUnauthorized: false,
+//         // },
+//         auth : {
+//             user : process.env.EMAIL_FROM,
+//             pass : process.env.EMAIL_PASSWORD
+//         }
+//     })
+
+//     const options = {
+//         from : process.env.EMAIL_FROM, 
+//         to, 
+//         subject, 
+//         text: message,
+//     }
+
+//     transporter.sendMail(options, (error, info) =>{
+//         if(error) console.log(error)
+//         else console.log(info)
+//     })
+
+// }
+const sendMail = async () => {
+    try {
+      const mailOptions = {
+        from: process.env.USER_EMAIL,
+        to: 'p.asq@live.com',
+        subject: "Test",
+        text: "Hi, this is a test email",
+      }
+ 
+      let emailTransporter = await createTransporter();
+      await emailTransporter.sendMail(mailOptions);
+    } catch (err) {
+      console.log("ERROR: ", err)
+    }
+  };
+ 
+const createTransporter = async () => {
+    try {
+      const oauth2Client = new OAuth2(
+          process.env.CLIENT_ID,
+          process.env.CLIENT_SECRET,
+          "http://localhost:5000"
+        );
+ 
+        oauth2Client.setCredentials({
+          refresh_token: process.env.REFRESH_TOKEN,
+        });
+ 
+        const accessToken = await new Promise((resolve, reject) => {
+          oauth2Client.getAccessToken((err, token) => {
+            if (err) {
+              console.log("*ERR: ", err)
+              reject();
+            }
+            resolve(token); 
+          });
+        });
+ 
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            type: "OAuth2",
+            user: process.env.USER_EMAIL,
+            accessToken,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+          },
+        });
+        return transporter;
+    } catch (err) {
+      return err
+    }
+  };
+  sendMail()
+
 // Register
 exports.register = async (req, res, next) => {
     const { username, password } = req.body
@@ -97,6 +193,55 @@ exports.login = async (req, res, next) => {
 }
 
 
+exports.resetPassword = async (req, res, next) => {
+    const { email } = req.body
+    // Check if username and password is provided
+    // if (!username || !password) {
+    //     return res.status(400).json({
+    //         message: "Username or Password not present",
+    //     })
+    // }
+
+    // try {
+    //     const user = await User.findOne({ email })
+    //     if (!user) {
+    //       res.status(400).json({
+    //         message: "Password reset not successful",
+    //         error: "User not found",
+    //       })
+    //     } else {
+    //       // comparing given password with hashed password
+    //     //   bcrypt.compare(password, user.password).then(function (result) {
+    //     //     if(result) {
+    //     //         const maxAge = 6*60*60;
+    //     //         const token = jwt.sign({id: user._id, username, role: user.role}, jwtSecret, {expiresIn: maxAge})
+    //     //         res.status(200).json({
+    //     //             message: "Login successful",
+    //     //             user,
+    //     //             access_token: token
+    //     //         })
+    //     //     } else {
+    //     //         res.status(400).json({ message: "Login not succesful" })
+    //     //     }
+    //     //     // result
+    //     //     //   ? res.status(200).json({
+    //     //     //       message: "Login successful",
+    //     //     //       user,
+    //     //     //       access_token: token
+    //     //     //     })
+    //     //     //   : res.status(400).json({ message: "Login not succesful" })
+    //     //   })
+    //         sendMail('aashish@mozej.com', 'Thank you!', 'Thank you so much for sticking with me');
+    //     }
+    //   } catch (error) {
+    //     res.status(400).json({
+    //       message: "An error occurred",
+    //       error: error.message,
+    //     })
+    //   }
+    sendMail('aashish@mozej.com', 'Thank you!', 'Thank you so much for sticking with me');
+
+}
 //auth.js
 exports.update = async (req, res, next) => {
     const { role, id } = req.body;
--- auth/route.js
+++ auth/route.js
@@ -1,13 +1,15 @@
 const express = require("express");
 // const mongoose = require("mongoose");
 // const User = require("../models/user");
-const { register, login, update, deleteUser } = require("./auth");
+const { register, login, resetPassword, update, deleteUser } = require("./auth");
 // const bcrypt = require("bcrypt");
 // var jwt = require("jsonwebtoken");
 const adminGuard = require('../middleware/admin-auth')
 const router = express.Router({ mergeParams: true });
 router.post("/register", register);
 router.post("/login", login);
+router.post("/resetpassword", resetPassword);
+
 router.put("/update", update);
 router.delete("/delete-user", adminGuard, deleteUser);
 
