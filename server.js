61439d4339ccb911afaca81b218544125163a1a5
margaret ogbor <p.asq@live.com>
2024-01-01 15:13:52 +0100

    Add /api/users route and admin guard

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const PORT = 5000;
 const cors = require('cors')
 const dotenv = require('dotenv');
+const basicAuth = require("./middleware/basic-auth");
+const adminAuth = require("./middleware/admin-auth");
 dotenv.config();
 
 mongoose.connect(`${process.env.MONGODB_URI}`, {
@@ -18,6 +20,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/auth", require("./auth/route"))
+app.use("/api/users", adminAuth, require("./core/route"))
+
 
 
 const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`));
